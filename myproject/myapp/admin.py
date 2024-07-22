125f2aa7f4ad492d2923e1ced6cb41b9e1477f82
Clinton26 <134915423+Wanyangu1@users.noreply.github.com>
2024-07-22 15:03:09 +0300

    wip: added progress

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,6 @@
+# myapp/admin.py
 from django.contrib import admin
+from .models import UserProfile, Item
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Item)
